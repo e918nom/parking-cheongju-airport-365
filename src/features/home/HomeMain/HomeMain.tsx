11e3fc9df62c5fcf0e9060debefcd63dd63e2074
e918nom <wlral2781@naver.com>
2025-09-20 03:09:54 +0900

    Update HomeMain.tsx

--- src/features/home/HomeMain/HomeMain.tsx
+++ src/features/home/HomeMain/HomeMain.tsx
@@ -31,8 +31,7 @@ export default function HomeMain() {
           <img src="/images/home/scroll.png" alt="마우스스크롤" />
         </div>
       </div>
-    </div>
-    <div className="hidden -z-10">
+      <div className="hidden -z-10">
      <h2><strong>청주공항 주차대행</strong>, 이제 고민 없이! <strong>청주국제공항 주차대행</strong>은 저희에게 맡기세요.</h2>
     <p>
         <strong>청주국제공항</strong>을 이용하실 때마다 주차 문제로 골치 아프셨나요? 복잡한 <strong>청주공항 주차장</strong>을 찾아 헤매는 시간, 무거운 짐을 들고 걷는 수고로움은 이제 잊으세요. 저희 <strong>청주공항 주차대행</strong> 서비스가 여러분의 여행 시작을 산뜻하게 만들어 드립니다. <strong>청주공항 주차대행</strong>은 단순한 편의를 넘어, 여러분의 소중한 시간을 절약해 드립니다.
@@ -74,5 +73,6 @@ export default function HomeMain() {
 <li>합리적인 <strong>청주공항 주차대행</strong> 요금으로 여행 경비를 절약할 수 있습니다. 특히 <strong>장기주차대행</strong> 시 더욱 큰 할인 혜택을 드립니다.</li>
 </ul>
 </div>
+    </div>
   );
 }
